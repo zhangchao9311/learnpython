0f73693ecbcfe8b08a6efac3c132dd33c5e64c08
张潮 <zhangchao@zhangchaodeMac-mini.local>
2025-10-19 21:49:18 +0800

    exercise_8.8

--- make_album.py
+++ make_album.py
@@ -1,15 +1,19 @@
 def make_album(singer_name, album_name, songs_number=None):
     """返回一个字典，其中包含一个音乐专辑的信息"""
     music_album={'singer': singer_name, 'album': album_name}
-    if songs_number:
-        music_album['songs_number'] = songs_number
     return music_album
 
-album_1 = make_album('Thriller', 'Michael Jackson', songs_number=9)
-print(album_1)
+while True:
+    print("\nPlease tell me your favorite album.")
+    print("(enter 'q' at any time to quit)")
 
-album_2 = make_album('1989', 'Taylor Swift')
-print(album_2)
+    a_name = input("Album name:")
+    if a_name == 'q':
+        break
 
-album_3 = make_album('good kid', 'Kendrick Lamar')
-print(album_3)
+    s_name = input("Singer name:")
+    if s_name == 'q':
+        break
+
+    get_music_album = make_album(a_name, s_name)
+    print(get_music_album)
