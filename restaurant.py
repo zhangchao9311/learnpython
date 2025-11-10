c726155802d025a2acaa5db027895bb31610b588
张潮 <zhangchao@zhangchaodeMac-mini.local>
2025-11-10 22:04:25 +0800

    add number_served exercise_9.4

--- restaurant.py
+++ restaurant.py
@@ -5,6 +5,7 @@ class Restaurant:
         """初始化属性name和type"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         """打印餐馆名称和餐馆风味"""
@@ -14,11 +15,18 @@ class Restaurant:
         """打印这家店是开张的"""
         print(f"The {self.restaurant_name} is openning!")
 
+    def served_number(self):
+        """打印餐馆就餐的人数"""
+        print(f"This restaurant has been served {self.number_served} people.")
+
+    def set_number_served(self, numbers):
+        """设置就餐人数"""
+        self.number_served = numbers
+
+
 restaurant = Restaurant('Hao Liyou', 'Chinese')
 restaurant.describe_restaurant()
+restaurant.open_restaurant()
 
-restaurant_1 = Restaurant('Tong Fu', 'Xiangcai')
-restaurant_1.describe_restaurant()
-
-restaurant_2 = Restaurant('A Qiang', 'Yuecai')
-restaurant_2.describe_restaurant()
+restaurant.set_number_served(210)
+restaurant.served_number()
